5ae54a4baa50f3624dd3df4a68c692e0fec9984c
oushimu256 <dev.051005.mb178@gmail.com>
2016-10-22 15:36:17 +0900

    memo
    xxxxxxxxx

---
+++ FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  aTemplateTabbed
+//
+//  Created by t.inaba on 2016/10/16.
+//  Copyright © 2016年 t.inaba. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
